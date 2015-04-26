35183608bbf4c5fe3abb6c8baa97b513a2e24066
Patrick Werner <pwerner2@gmail.com>
2015-04-25 22:31:12 -0400

    addendum to last commit

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,19 +1,5 @@
-var postsData = [ {
-    title: 'Funny',
-    url: 'http://lolcats.com/'
-  },
-  {
-    title: 'Reddit',
-    url: 'http://reddit.com'
-}, {
-    title: 'MeteorJS',
-    url: 'http://meteor.com'
-  },
-  {
-    title: 'Job Searches',
-    url: 'http://indeed.com'
-} 
-];
 Template.postsList.helpers({
-  posts: postsData
+    posts: function() {
+        return Posts.find();
+    }
 });
