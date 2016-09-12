eb19af2f19018061b81839d6a06f2e9689974e22
Patrick Werner <pwerner2@gmail.com>
2016-09-11 22:16:18 -0400

    restarted work on project. Iron router now working, 404 working.

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,17 +1,17 @@
-if (Posts.find().count() === 0) {
+//if (Posts.find().count() === 0) {
     
-    Posts.insert({
-        title: 'Learn to Code',
-        url: 'http://theironyard.com' 
-    });
+    //Posts.insert({
+        //title: 'Find some work',
+        //url: 'http://gun.io' 
+    //});
 
-    Posts.insert ({
-        title: 'Waste some time',
-        url: 'http://reddit.com'
-    });
+    //Posts.insert ({
+        //title: 'Waste some time',
+        //url: 'http://reddit.com'
+    //});
 
-    Posts.insert ({
-        title: 'Cool Post',
-        url: 'http://google.com'
-    });
-}
+    //Posts.insert ({
+        //title: 'Cool Post',
+        //url: 'http://google.com'
+    //});
+//}
