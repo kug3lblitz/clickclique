c08547429aaaae6fb45b041f3dd63910c3ec1592
Patrick Werner <pwerner2@gmail.com>
2016-09-18 21:50:25 -0400

    post timestamps, added security

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,11 +1,31 @@
 Template.postSubmit.events({ 'submit form': function(e) {
     e.preventDefault();
-
+    
     var post = {
     url: $(e.target).find('[name=url]').val(), title: $(e.target).find('[name=title]').val()
     };
 
-    post._id = Posts.insert(post);
-        Router.go('postPage', post);
-  }
+    Meteor.call('postInsert', post, function(error, result) { // display the error to the user and abort
+      if (error)
+        return alert(error.reason);
+        Router.go('postPage', {_id: result._id});
+    });
+  } 
+});
+
+Template.postSubmit.events({ 'submit form': function(e) {
+    e.preventDefault();
+    var post = {
+    url: $(e.target).find('[name=url]').val(), title: $(e.target).find('[name=title]').val()
+    };
+
+    Meteor.call('postInsert', post, function(error, result) { // display the error to the user and abort
+    if (error)
+      return alert(error.reason);
+      // show this result but route anyway
+      if (result.postExists)
+      alert('This link has already been posted');
+      Router.go('postPage', {_id: result._id});
+    });
+  } 
 });
--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,5 +1,6 @@
-Template.postsList.helpers({
-    posts: function() {
-        return Posts.find();
-    }
+Template.postsList.helpers({ 
+  posts: function() {
+      return Posts.find({}, {
+        sort: {submitted: -1}}); 
+  }
 });
