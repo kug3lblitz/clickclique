5b8263e52ddd8bc816a7adba95406fd46f40bff2
Patrick Werner <pwerner2@gmail.com>
2016-10-04 16:12:12 -0400

    added post validation to client/server

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -20,6 +20,10 @@ Template.postSubmit.events({
       title: $(e.target).find('[name=title]').val()
     };
 
+    var errors = validatePost(post); 
+    if (errors.title || errors.url)
+      return Session.set('postSubmitErrors', errors);
+
     Meteor.call('postInsert', post, function(error, result) { 
     // display the error to the user and abort
     if (error)
