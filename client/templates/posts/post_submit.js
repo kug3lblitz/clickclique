9c9f5a8a5ffab9e951e40f933ba059cf9345b3cc
Patrick Werner <pwerner2@gmail.com>
2016-10-03 23:40:38 -0400

    enhanced error messages

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,16 @@
+Template.postSubmit.created = function() { 
+  Session.set('postSubmitErrors', {});
+}
+
+Template.postSubmit.helpers({ 
+  errorMessage: function(field) {
+    return Session.get('postSubmitErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+  } 
+});
+
 Template.postSubmit.events({ 
   'submit form': function(e) {
     e.preventDefault();
